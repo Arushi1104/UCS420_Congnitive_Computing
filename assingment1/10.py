e17140ac863e16d073f77b55712a3317c16a1137
UCS712 <arushikhanna1025@gmail.com>
2025-01-12 23:23:05 +0530

    edit

--- assingment1/10.py
+++ assingment1/10.py
@@ -1,17 +1,15 @@
 
 #10.1
 import sys
+
 if len(sys.argv) != 3:
-    print("Usage:python intro_to_python.py <num1> <num2>")
+    print("Usage: python 10.py <num1> <num2>")
     sys.exit(1)
 
-try:
-    num1 = float(sys.argv[1])
-    num2 = float(sys.argv[2])
+num1 = float(sys.argv[1])
+num2 = float(sys.argv[2])
 
-    print(f"The sum of {num1} and {num2} is {num1 + num2}")
-except ValueError:
-    print("Error: Both arguments must be numbers.")
+print(f"The sum of {num1} and {num2} is {num1 + num2}")
 
 #10.2
 import sys
